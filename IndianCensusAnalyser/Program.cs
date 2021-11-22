571113d0597cec5dbf5f169345e530b0c2a2d035
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-11-22 12:35:06 +0530

    [VIGNESH M] Added: Welcome Message

---
+++ IndianCensusAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Indian Census Analyser");
+        }
+    }
+}
